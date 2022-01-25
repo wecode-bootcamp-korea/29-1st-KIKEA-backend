5f22b12fb9933755628ae6033789bc279bf9d3c6
newdana01 <newdana01@gmail.com>
2022-01-25 21:37:20 +0900

    [Modify] 컨벤션 수정

--- order/models.py
+++ order/models.py
@@ -33,9 +33,9 @@ class ShippingStatus(models.Model):
         db_table = 'shipping_status'
 
 class Cart(TimeStampModel):
-    user          = models.ForeignKey('User', on_delete=models.CASCADE)
+    user           = models.ForeignKey('User', on_delete=models.CASCADE)
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
-    quantity      = models.PositiveIntegerField()
+    quantity       = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'carts'
