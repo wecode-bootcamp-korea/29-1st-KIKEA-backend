bad2134a28b7402dce5eba36077f09658ee1b812
shawn365 <eogkr.kim@gmail.com>
2022-01-25 19:17:48 +0900

    create: products modeling

---
+++ products/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    class Meta:
+        abstract = True
+
+class User(TimeStampModel):
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=100)
+    point        = models.PositiveIntegerField()
+    class Meta:
+        db_table = 'users'
+
+class Cart(TimeStampModel):
+    user          = models.ForeignKey('User', on_delete=models.CASCADE)
+    productoption = models.ForeignKey('ProductOption', on_delete=models.CASCADE)
+    quantity      = models.PositiveIntegerField()
+    class Meta:
+        db_table = 'carts'
+
+class ProductOption(TimeStampModel):
+    price   = models.PositiveBigIntegerField()
+    stock   = models.TextField()
+    color   = models.ForeignKey('Color', on_delete=models.CASCADE)
+    size    = models.ForeignKey('Size', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'product_options'
+
+class Product(TimeStampModel):
+    name          = models.CharField(max_length=45)
+    description   = models.TextField()
+    default_image = models.URLField()
+    subcategory   = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'products'
+
+class Image(TimeStampModel):
+    image_url     = models.URLField(max_length=500)
+    productoption = models.ForeignKey('ProductOption', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'images'
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'categories'
+
+class SubCategory(models.Model):
+    name     = models.CharField(max_length=45)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'sub_categories'
+
+class Review(TimeStampModel):
+    comment = models.TextField(max_length=500)
+    rating  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    user    = models.ForeignKey('User', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'reviews'
+
+class Color(TimeStampModel):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'colors'
+
+class Size(TimeStampModel):
+    product_size = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'sizes'
