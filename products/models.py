111337504d50a3fe550daf6e3b8be9fda54d9da5
shawn365 <eogkr.kim@gmail.com>
2022-01-26 13:30:58 +0900

    create: products modeling

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+from django.db              import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from users.models import TimeStampModel
+from users.models           import TimeStampModel
 
 class ProductOption(TimeStampModel):
     price   = models.PositiveBigIntegerField()
@@ -8,6 +8,7 @@ class ProductOption(TimeStampModel):
     color   = models.ForeignKey('Color', on_delete=models.CASCADE)
     size    = models.ForeignKey('Size', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    
     class Meta:
         db_table = 'product_options'
 
@@ -16,23 +17,27 @@ class Product(TimeStampModel):
     description   = models.TextField()
     default_image = models.URLField()
     subcategory   = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+    
     class Meta:
         db_table = 'products'
 
 class Image(TimeStampModel):
     image_url     = models.URLField(max_length=500)
     productoption = models.ForeignKey('ProductOption', on_delete=models.CASCADE)
+    
     class Meta:
         db_table = 'images'
 
 class Category(TimeStampModel):
     name = models.CharField(max_length=45)
+
     class Meta:
         db_table = 'categories'
 
 class SubCategory(models.Model):
     name     = models.CharField(max_length=45)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
     class Meta:
         db_table = 'sub_categories'
 
@@ -41,15 +46,18 @@ class Review(TimeStampModel):
     rating  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
+
     class Meta:
         db_table = 'reviews'
 
 class Color(TimeStampModel):
     name = models.CharField(max_length=45)
+
     class Meta:
         db_table = 'colors'
 
 class Size(TimeStampModel):
     product_size = models.CharField(max_length=45)
+
     class Meta:
         db_table = 'sizes'
