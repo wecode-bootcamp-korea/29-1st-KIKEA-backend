4df7f0e199c30af6f4cb62c0ec26725544f9789d
newdana01 <newdana01@gmail.com>
2022-01-26 17:44:02 +0900

    [Modify] models.py

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from users.models import TimeStampModel
+from cores.models import TimeStampModel
 
 class Order(TimeStampModel):
     order_number = models.CharField(max_length=100, unique=True)
