b9b49ff001c0a3e8eb16312aaf0eec6f60b7faf0
shawn365 <eogkr.kim@gmail.com>
2022-01-26 17:23:41 +0900

    create: users modeling

--- users/models.py
+++ users/models.py
@@ -1,13 +1,7 @@
+from pyexpat import model
 from django.db import models
 
-class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-
-    class Meta:
-        abstract = True
-
-class User(TimeStampModel):
+class User(models.Model):
     name         = models.CharField(max_length=45)
     email        = models.EmailField(max_length=100, unique=True)
     password     = models.CharField(max_length=200)
