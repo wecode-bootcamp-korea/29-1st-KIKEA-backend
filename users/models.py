6390a9670a4ba0829ae18ba90a706ea7b0a4d895
shawn365 <eogkr.kim@gmail.com>
2022-01-26 13:29:35 +0900

    create: users modeling

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        abstract = True
+
+class User(TimeStampModel):
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=100)
+    point        = models.PositiveIntegerField()
+    
+    class Meta:
+        db_table = 'users'
