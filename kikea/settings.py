b3c75562a66da0dd35f49392099cca84ce3356f8
newdana01 <newdana01@gmail.com>
2022-01-27 17:50:27 +0900

    [Modify] settings.py

--- kikea/settings.py
+++ kikea/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib     import Path
 from my_settings import DATABASES, SECRET_KEY
-# import pymysql
+import pymysql
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 DATABASES = DATABASES
         
