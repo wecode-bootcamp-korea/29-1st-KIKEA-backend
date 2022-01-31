b98899e789d5c7352794a6c6e05d78ff171864aa
newdana01 <newdana01@gmail.com>
2022-02-01 00:49:47 +0900

    [Add] CartView - get() 작성

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http  import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Cart
+
+class CartView(View):
+    def get(self, request):
+        user_id = request.user.id
+        carts   = Cart.objects.filter(user=user_id)
+
+        if not carts.exists():
+            return JsonResponse({'message': 'NO_PRODUCT'}, status=200)
+
+        result = []
+
+        for cart in carts:
+            color = 'NULL'
+            size  = 'NULL'
+
+            if cart.product_option.color != None:
+                color = cart.product_option.color.name
+            if cart.product_option.size != None:
+                size = cart.product_option.size.name
+
+            result.append(
+                {
+                    'name'         : cart.product_option.product.name,
+                    'type'         : cart.product_option.product.type.name,
+                    'color'        : color,
+                    'size'         : size,
+                    'quantity'     : cart.quantity,
+                    'price'        : cart.product_option.price,
+                    'default_image': cart.product_option.product.default_image
+                }
+            )
+
+        return JsonResponse({'result':result}, status=200)
