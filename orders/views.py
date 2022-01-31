a2b2d133dd33d969d44d0c8cd30b043a4089aaa0
newdana01 <newdana01@gmail.com>
2022-02-01 01:17:50 +0900

    [Add] CartView - put() 작성

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import HttpResponse
+from django.views import View
+
+from .models import Cart
+
+class CartView(View):
+    def put(self, request):
+        data = json.loads(request.body)
+
+        user_id  = request.user.id
+        quantity = data['quantity']
+
+        cart = Cart.objects.filter(user=user_id, product_option=data['product_option_id'])
+
+        if quantity == '0':
+            cart.delete()
+            return HttpResponse(status=204)
+
+        cart.update(quantity=quantity)
+
+        return HttpResponse(status=200)
