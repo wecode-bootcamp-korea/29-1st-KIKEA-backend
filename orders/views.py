00e55d46724983ec1bdd59660d1c085c79d0a4bd
newdana01 <newdana01@gmail.com>
2022-02-01 18:44:31 +0900

    [Modify] 코드 수정

--- orders/views.py
+++ orders/views.py
@@ -1,26 +1,31 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse, JsonResponse
 from django.views import View
 
 from .models         import Cart
 from users.models    import User
+from products.models import ProductOption
 
 class CartView(View):
     def post(self, request, product_option_id):
-        quantity = 1
+        try:
+            quantity = 1
 
-        user = User.objects.get(id=request.user.id)
-        cart = Cart.objects.filter(user=user.id, product_option=product_option_id)
-        
-        if cart.exists():
-            quantity += cart[0].quantity
-            cart.update(quantity=quantity)
+            user           = User.objects.get(id=request.user.id)
+            product_option = ProductOption.objects.get(id=product_option_id)
+            cart           = Cart.objects.filter(user=user.id, product_option=product_option.id)
+            
+            if cart.exists():
+                quantity += cart[0].quantity
+                cart.update(quantity=quantity)
 
-            return HttpResponse(status=204)
+                return HttpResponse(status=204)
 
-        Cart.objects.create(
-            quantity       = quantity,
-            product_option = product_option_id,
-            user           = user
-            )
-        
-        return HttpResponse(status=201)
+            Cart.objects.create(
+                quantity       = quantity,
+                product_option = product_option,
+                user           = user
+                )
+            
+            return HttpResponse(status=201)
+        except ProductOption.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_PRODUCT_OPTION'}, status=400)
