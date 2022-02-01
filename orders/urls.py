6a90c9d58fa943231ad285f1bb0f37fd92570d98
newdana01 <newdana01@gmail.com>
2022-02-01 17:45:26 +0900

    [Modify] 리팩토링-RESTful API

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from .views import CartView
 
 urlpatterns = [
     path('/carts', CartView.as_view()),
+    path('/carts/<int:product_option_id>', CartView.as_view()),
     ]
--- orders/views.py
+++ orders/views.py
@@ -1,35 +1,28 @@
 import json
 
-from django.http  import HttpResponse, JsonResponse
+from django.http  import HttpResponse
 from django.views import View
 
 from .models         import Cart
 from users.models    import User
-from products.models import ProductOption
 
 class CartView(View):
-    def post(self, request):
-        data = json.loads(request.body)
+    def post(self, request, product_option_id):
+        quantity = 1
 
-        try:
-            quantity = 1
+        user = User.objects.get(id=request.user.id)
+        cart = Cart.objects.filter(user=user.id, product_option=product_option_id)
+        
+        if cart.exists():
+            quantity += cart[0].quantity
+            cart.update(quantity=quantity)
 
-            user           = User.objects.get(id=request.user.id)
-            product_option = ProductOption.objects.get(id=data['product_option_id'])
-            cart           = Cart.objects.filter(user=user.id, product_option=product_option.id)
-            
-            if cart.exists():
-                quantity += cart[0].quantity
-                cart.update(quantity=quantity)
+            return HttpResponse(status=204)
 
-                return HttpResponse(status=204)
-
-            Cart.objects.create(
-                quantity       = quantity,
-                product_option = product_option,
-                user           = user
-                )
-            
-            return HttpResponse(status=201)
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        Cart.objects.create(
+            quantity       = quantity,
+            product_option = product_option_id,
+            user           = user
+            )
+        
+        return HttpResponse(status=201)
