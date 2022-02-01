4ee0ee04992f9496a94bf292bcd7b84d5563145a
newdana01 <newdana01@gmail.com>
2022-02-01 18:51:27 +0900

    [Add] CartView - delete() 작성

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http  import HttpResponse
+from django.views import View
 
-# Create your views here.
+from .models         import Cart
+
+class CartView(View):
+    def delete(self, request, product_option_id):
+        Cart.objects.filter(
+            user           = request.user.id,
+            product_option = product_option_id
+            ).delete()
+
+        return HttpResponse(status=204)
