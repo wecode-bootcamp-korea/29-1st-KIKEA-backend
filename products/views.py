ef362ed68b6c349889b07aa4b07bdf7d211e5d58
shawn365 <eogkr.kim@gmail.com>
2022-02-06 01:02:33 +0900

    create: category view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views           import View
+from django.http            import JsonResponse, HttpResponse
 
-# Create your views here.
+from .models                import *
+
+class CategoryView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+
+        results = [{
+            "id" : category.id,
+            "name" : category.name,
+            "subcategory_list" : [{
+                "id" : subcategory.id,
+                "name" : subcategory.name,
+                "type_list" : [{
+                    "id" : type.id,
+                    "name" : type.name,
+                    "product_list" : [{
+                        "id" : product.id,
+                        "name" : product.name,
+                        "description" : product.description,
+                        "default_image" : product.default_image,
+                    } for product in type.product_set.all()]
+                } for type in subcategory.type_set.all()]                    
+            } for subcategory in category.subcategory_set.all()]
+        } for category in categories]
+
+        return JsonResponse({"categories" : results}, status = 200)
+        
