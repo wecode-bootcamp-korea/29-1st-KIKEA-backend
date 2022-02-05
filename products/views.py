aa3da5240c4c7ee9751dad8083f0cba3b7ddd812
shawn365 <eogkr.kim@gmail.com>
2022-02-06 01:11:19 +0900

    create: product option view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.views           import View
+from django.http            import JsonResponse, HttpResponse
+from django.db.models       import Q
 
-# Create your views here.
+from .models                import *
+
+class ProductOptionView(View):
+    def get(self, request):
+        try:
+            category_name    = request.GET.getlist('category', None)
+            subcategory_name = request.GET.getlist('subcategory', None)
+            type_name        = request.GET.getlist('type', None)
+            product_name     = request.GET.getlist('product')
+            sorting          = request.GET.get('sort', None)
+
+            q = Q()
+
+            if category_name:
+                q &= Q(product__type__subcategory__category__name__in = category_name)
+            
+            if subcategory_name:
+                q &= Q(product__type__subcategory__name__in = subcategory_name)
+
+            if type_name:
+                q &= Q(product__type__name__in = type_name)
+            
+            if product_name:
+                q &= Q(product__name__in = product_name)
+            
+            productoptions = ProductOption.objects.filter(q)
+            
+            if sorting:
+                productoptions = productoptions.order_by(sorting)
+
+            result = [{
+                "id"         : productoption.id,
+                "price"      : productoption.price,
+                "stock"      : productoption.stock,
+                "color_id"   : productoption.color_id,
+                "size_id"    : productoption.size_id,
+                "product_id" : productoption.product_id,
+                "image"      : [image_url.image_url for image_url in productoption.productoptionimage_set.all()]
+            }for productoption in productoptions]
+
+            return JsonResponse({'message':'SUCCESS' ,'result' : result}, status=200)
+        except Exception as e:
+            return JsonResponse({'message': type(e)}, status=400)
