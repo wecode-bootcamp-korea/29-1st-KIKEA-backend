8273fefa86edc6b60772008da8d8f67afda62742
shawn365 <eogkr.kim@gmail.com>
2022-02-07 21:50:35 +0900

    create: type view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views           import View
+from django.http            import JsonResponse, HttpResponse
 
-# Create your views here.
+from .models                import *
+
+class TypeView(View):
+    def get(self, request):
+        types = Type.objects.filter(subcategory__name__in = request.GET.getlist('subcategory'))
+
+        results = [{
+            "id"        : type.id,
+            "name"      : type.name,
+            "image_url" : type.image_url,
+        } for type in types]
+
+        return JsonResponse({"categories" : results}, status = 200)
+        
