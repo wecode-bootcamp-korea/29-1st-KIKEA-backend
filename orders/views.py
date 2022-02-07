ffb8b57c78ce8a841711def4965790a1e9c9c039
newdana01 <newdana01@gmail.com>
2022-02-07 21:53:48 +0900

    [Modify] get 리팩토링

--- orders/views.py
+++ orders/views.py
@@ -2,40 +2,28 @@ from django.http  import JsonResponse
 from django.views import View
 
 from .models     import Cart
-from users.utils import @login_decorator
+from users.utils import login_decorator
 
 class CartView(View):
     @login_decorator
     def get(self, request):
-        user_id = request.user.id
-        carts   = Cart.objects.filter(user=user_id)
+        carts   = Cart.objects.filter(user=1)
 
         if not carts.exists():
             return JsonResponse({'message': 'NO_PRODUCT'}, status=200)
 
-        result = []
-
-        for cart in carts:
-            color = 'NULL'
-            size  = 'NULL'
-
-            if cart.product_option.color != None:
-                color = cart.product_option.color.name
-            if cart.product_option.size != None:
-                size = cart.product_option.size.name
-
-            result.append(
-                {
-                    'product_option_id'   : cart.product_option.id,
-                    'name'                : cart.product_option.product.name,
-                    'type'                : cart.product_option.product.type.name,
-                    'color'               : color,
-                    'size'                : size,
-                    'quantity'            : cart.quantity,
-                    'price'               : cart.product_option.price,
-                    'total_price'         : cart.quantity * cart.product_option.price,
-                    'default_image'       : cart.product_option.product.default_image
-                }
-            )
+        result = [
+            {
+                'product_option_id' : cart.product_option.id,
+                'name'              : cart.product_option.product.name,
+                'type'              : cart.product_option.product.type.name,
+                'color'             : cart.product_option.color.name if cart.product_option.color else None,
+                'size'              : cart.product_option.size.name if cart.product_option.size else None,
+                'quantity'          : cart.quantity,
+                'price'             : cart.product_option.price,
+                'total_price'       : cart.quantity * cart.product_option.price,
+                'default_image'     : cart.product_option.product.default_image
+            } for cart in carts
+        ]
             
         return JsonResponse({'result':result}, status=200)
