2ad0b3da290b23cfcca4845cc485c796eb122ec9
shawn365 <eogkr.kim@gmail.com>
2022-02-07 21:48:12 +0900

    modify: category_view

--- products/views.py
+++ products/views.py
@@ -8,23 +8,12 @@ class CategoryView(View):
         categories = Category.objects.all()
 
         results = [{
-            "id" : category.id,
-            "name" : category.name,
+            "id"               : category.id,
+            "name"             : category.name,
             "subcategory_list" : [{
-                "id" : subcategory.id,
-                "name" : subcategory.name,
-                "type_list" : [{
-                    "id" : type.id,
-                    "name" : type.name,
-                    "product_list" : [{
-                        "id" : product.id,
-                        "name" : product.name,
-                        "description" : product.description,
-                        "default_image" : product.default_image,
-                    } for product in type.product_set.all()]
-                } for type in subcategory.type_set.all()]                    
+                "id"           : subcategory.id,
+                "name"         : subcategory.name,                                 
             } for subcategory in category.subcategory_set.all()]
         } for category in categories]
 
         return JsonResponse({"categories" : results}, status = 200)
-        
