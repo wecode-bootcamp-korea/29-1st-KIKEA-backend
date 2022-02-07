25d79c180457636afae05815b28f0ff38b6e9a1b
newdana01 <newdana01@gmail.com>
2022-02-08 00:06:35 +0900

    [Modify] 데코레이터 적용

--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,13 @@ from django.http  import HttpResponse
 from django.views import View
 
 from .models         import Cart
+from users.utils     import login_decorator
 
 class CartView(View):
+    @login_decorator
     def delete(self, request, product_option_id):
         Cart.objects.filter(
-            user           = request.user.id,
+            user           = request.user,
             product_option = product_option_id
             ).delete()
 
