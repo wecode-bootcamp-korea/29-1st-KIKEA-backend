6e9b54a366955c90bdd3c2a186daec70dd7693f6
newdana01 <newdana01@gmail.com>
2022-02-09 02:10:57 +0900

    [Add] OrderView-patch()

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from orders.views import CartView
+from orders.views import CartView, OrderView
 
 urlpatterns = [
+    path('/<int:order_item_id>', OrderView.as_view()),
     path('/carts', CartView.as_view()),
     path('carts/<int:product_option_id>', CartView.as_view()),
     ]
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,47 @@
 import json
 
-from django.http  import JsonResponse, HttpResponse
-from django.views import View
+from django.http      import JsonResponse, HttpResponse
+from django.views     import View
+from django.db        import transaction
+from django.db.models import F, Sum
+from django.utils     import timezone
 
-from .models         import Cart
+from .models import (
+    Cart,
+    OrderStatus,
+    OrderItem,
+    ShippingStatus
+    )
 from users.utils     import login_decorator
 from products.models import ProductOption
+
+class OrderView(View):
+    @login_decorator
+    def patch(self, request, order_item_id):
+        order_items = OrderItem.objects.filter(id=order_item_id)
+        order_item = order_items[0]
+        
+        if (timezone.now() - order_item.order.created_at).days > 7:
+            return JsonResponse({'message': 'NOT_AVAILABLE'}, status=400)
+        
+        with transaction.atomic():
+            total_payment = order_items.aggregate(
+                total = Sum(F('quantity') * F('product_option__price'))
+                )['total']
+
+            order_items.update(
+                shipping_status = ShippingStatus.objects.get(name='회수 요청')
+                )
+            
+            order_item.order.order_status_id = OrderStatus.objects.get(name='부분 취소')
+            order_item.product_option.stock += order_item.quantity
+            order_item.user.point           += total_payment
+
+            order_item.order.save()
+            order_item.product_option.save()
+            order_item.user.save()
+        
+        return HttpResponse(status=200)
 
 class CartView(View):
     @login_decorator
