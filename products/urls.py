92058a054cc28fa4a4be27557ecd32661964bae2
shawn365 <eogkr.kim@gmail.com>
2022-02-08 16:21:00 +0900

    resolved commit

--- products/urls.py
+++ products/urls.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-from django.urls import path
-
-from .views      import *
-
-urlpatterns = [
-    path("/review", ReviewView.as_view())
-]
-=======
 from django.urls import path
 
 from .views      import *
 
 urlpatterns = [
     path("/category", CategoryView.as_view()),
+    path("/review", ReviewView.as_view())
 ]
->>>>>>> main
