ba54ad89c9796870943e1d5f66a857fd58acff47
shawn365 <eogkr.kim@gmail.com>
2022-02-09 13:35:48 +0900

    resolved commit

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from .views      import *
 
 urlpatterns = [
-<<<<<<< HEAD
     path("/type", TypeView.as_view()), 
-=======
     path("", ProductOptionView.as_view()),
->>>>>>> main
     path("/category", CategoryView.as_view()),
 ]
